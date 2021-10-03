191b0c25c028f21615e57f7f9668a00e43342ec4
yushan <yushan@intervest.lk>
2021-10-03 12:05:27 +0530

    feat: Create the first spring using useSpring hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSpring } from "react-spring";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="logo" />
-          <button className="menu-button">Menu</button>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const fade = useSpring({
+    from: {
+      opacity: 0,
+    },
+    to: {
+      opacity: 1,
+    },
+  });
+
+  console.log(fade);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" alt="" />
+        <button className="menu-button">Menu</button>
+      </header>
+    </div>
+  );
+};
 
 export default App;
