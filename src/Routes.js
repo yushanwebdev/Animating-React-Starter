2f254c30e003cd129c54bfe9a02915a882f92d20
yushan <yushan@intervest.lk>
2021-10-06 16:42:55 +0530

    fix: Fix item position issue when transition

--- src/Routes.js
+++ src/Routes.js
@@ -25,9 +25,14 @@ const Main = () => {
   const location = useLocation();
 
   const transitions = useTransition(location, (location) => location.key, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
+    from: {
+      opacity: 0,
+      position: "absolute",
+      width: "100%",
+      transform: "translate3d(100%, 0, 0)",
+    },
+    enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%, 0, 0)" },
   });
 
   return transitions.map(({ item, key, props: transition }) => (
