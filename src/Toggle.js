3719784e131e6a183b03975034d7531636436fd2
yushan <yushan@intervest.lk>
2021-10-06 12:18:59 +0530

    feat: Transition between elements when toggle

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,20 +4,23 @@ import { useTransition, animated } from "react-spring";
 const Toggle = () => {
   const [isToggle, setToggle] = useState(false);
   const transition = useTransition(isToggle, null, {
-    from: { opacity: 0 },
+    from: { opacity: 0, position: "absolute" },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
   });
 
   return (
-    <div>
-      {transition.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.h1 key={key} style={props}>
-              Hello
-            </animated.h1>
-          )
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      {transition.map(({ item, key, props }) =>
+        item ? (
+          <animated.h1 style={props}>Hello</animated.h1>
+        ) : (
+          <animated.h1 style={props}>World</animated.h1>
+        )
       )}
       <button onClick={() => setToggle(!isToggle)}>Toggle</button>
     </div>
