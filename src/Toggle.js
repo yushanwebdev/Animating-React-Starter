96c6971331f10ec729040440a456b126a710a463
yushan <yushan@intervest.lk>
2021-10-06 12:31:19 +0530

    feat: Transition between list and a single component

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,9 +2,30 @@ import React, { useState } from "react";
 import { useTransition, animated } from "react-spring";
 
 const Toggle = () => {
-  const [isToggle, setToggle] = useState(false);
-  const transition = useTransition(isToggle, null, {
-    from: { opacity: 0, position: "absolute" },
+  const [items, setItems] = useState([
+    {
+      key: 1,
+      letter: "S",
+    },
+    {
+      key: 2,
+      letter: "c",
+    },
+    {
+      key: 3,
+      letter: "o",
+    },
+    {
+      key: 4,
+      letter: "t",
+    },
+    {
+      key: 5,
+      letter: "t",
+    },
+  ]);
+  const transition = useTransition(items, (items) => items.key, {
+    from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
   });
@@ -15,14 +36,23 @@ const Toggle = () => {
         position: "relative",
       }}
     >
-      {transition.map(({ item, key, props }) =>
-        item ? (
-          <animated.h1 style={props}>Hello</animated.h1>
-        ) : (
-          <animated.h1 style={props}>World</animated.h1>
-        )
-      )}
-      <button onClick={() => setToggle(!isToggle)}>Toggle</button>
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
+      <button
+        onClick={() =>
+          setItems([
+            {
+              key: "1",
+              letter: "Y",
+            },
+          ])
+        }
+      >
+        Toggle
+      </button>
     </div>
   );
 };
